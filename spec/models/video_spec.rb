03f2b6f868d614268043e89a0f307ecc9abc76da
David Ladowitz <david@ladowitz.com>
2016-05-18 10:53:07 -0700

    adding specs for video model

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Video do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "creates an instance of a video" do
+    video = Video.new
+    expect(video).to be_instance_of Video
+  end
+
+  it "has a title" do
+    video = Video.new(title: "Captain America: Civil War")
+    expect(video.title).to eq "Captain America: Civil War"
+  end
 end
