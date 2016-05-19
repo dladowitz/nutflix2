a095012228297be4b08ff4de19fd7574a5b245a1
David Ladowitz <david@ladowitz.com>
2016-05-19 14:54:33 -0700

    fixing root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Myflix::Application.routes.draw do
-  get 'genre(/:id)', to: "categories#show", as: :categories
+  root to: "videos#index"
+
+  get "genre(/:id)", to: "categories#show", as: :categories
 
   # root to: 'videos/index'
   resources :videos, only: [:index, :show]
 
-  get 'ui(/:action)', controller: 'ui'
+  get "ui(/:action)", controller: "ui"
 end
