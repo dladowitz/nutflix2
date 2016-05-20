db3dac95f974d497843983acde0efa47a7cbe8c5
David Ladowitz <david@ladowitz.com>
2016-05-20 11:00:57 -0700

    starting on Category#recent_videos

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,9 @@ class Category < ActiveRecord::Base
   has_many :videos
 
   validates :name, presence: true
+
+  # Instance Methods
+  def recent_videos
+    videos.order(created_at: :desc)#.limit(11)
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -23,4 +23,28 @@ describe Category do
       expect(category).not_to be_valid
     end
   end
+
+  describe "#recent_videos" do
+    before do
+      @action = Category.create(name: "Action")
+      @videos = []
+      5.times { |index| @videos << Video.create(title: "Iron Man #{index}", category: @action)}
+    end
+
+    subject { @action.recent_videos }
+
+    it "retunrs the videos in reverse chronological creation order" do
+      expect(subject.first.id).to eq @videos.last.id
+      expect(subject.last.id).to eq @videos.first.id
+    end
+
+    context "when there are 6 or less videos in the category" do
+
+      it "returns the exact number of videos in the category"
+    end
+
+    context "when there are more than 6 vidoes in the category" do
+      it "returns only 6 videos"
+    end
+  end
 end
