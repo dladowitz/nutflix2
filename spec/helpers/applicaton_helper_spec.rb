1795e97fdcb3d445d796b258466642392faf5658
David Ladowitz <david@ladowitz.com>
2016-05-20 17:21:32 -0700

    adding #current_user and specs

---
+++ spec/helpers/applicaton_helper_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+  describe "#current_user" do
+    before do
+      @tony = User.create(email: "tony@stark_labs.com", password: "asdfasdf", password_confirmation: "asdfasdf", full_name: "Tony Stark")
+    end
+
+    context "when a user is logged in" do
+      before { session[:id] = @tony.id }
+
+      it "returns the correct user" do
+        expect(current_user).to eq @tony
+      end
+    end
+
+    context "when no user is logged in" do
+      it "returns nil" do
+        expect(current_user).to eq nil
+      end
+    end
+  end
+end
