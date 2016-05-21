868377b71eca4b1936bea65d8948721f5a56ce33
David Ladowitz <david@ladowitz.com>
2016-05-21 11:30:11 -0700

    adding #verify_user to application controller

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -1,95 +1,134 @@
 require 'spec_helper'
 
 describe VideosController do
+  before do
+    @tony = User.create(email: "tony@stark_labs.com", password: "asdfasdf", password_confirmation: "asdfasdf", full_name: "Tony Stark")
+    action = Category.create(name: "Action")
+    @thor = Video.create(title: "Thor", category: action)
+  end
+
   describe "GET 'index'" do
-    before {
-      action = Category.create(name: "Action")
-      @thor = Video.create(title: "Thor", category: action)
-    }
+    subject { get 'index' }
 
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+    context "with a logged in user" do
+      before do
+        session[:id] = @tony.id
+        subject
+      end
+
+      it "returns http success" do
+        response.should be_success
+      end
+
+      it "retuns the index template" do
+        expect(response).to render_template :index
+      end
+
+      it "returns a collection of all categories in the DB" do
+        expect(assigns(:categories)).to eq Category.all
+      end
+
+      it "returns a list with categries full of vidoes" do
+        expect(assigns(:categories_w_videos)["Action"]).to eq [@thor]
+      end
     end
 
-    it "retuns the index template" do
-      get 'index'
-      expect(response).to render_template :index
-    end
+    context "with no user logged in" do
+      before { subject }
 
-    it "returns a collection of all categories in the DB" do
-      get 'index'
-      expect(assigns(:categories)).to eq Category.all
-    end
-
-    it "returns a list with categries full of vidoes" do
-      get 'index'
-      expect(assigns(:categories_w_videos)["Action"]).to eq [@thor]
+      it "redirects_to the sign_in_path" do
+        expect(response).to redirect_to sign_in_path
+        expect(flash[:danger]).to eq "Slow down there, you need to sign in first."
+      end
     end
   end
 
   describe "GET 'Show'" do
-    before {
-      @video = Video.create(title: "Thor", category_id: 2)
-    }
+    subject { get 'show', id: @thor.id }
 
-    it "returns http succss" do
-      get 'show', id: @video.id
-      response.should be_success
+    context "with a logged in user" do
+      before {
+        session[:id] = @tony.id
+        subject
+      }
+
+      it "returns http succss" do
+        response.should be_success
+      end
+
+      it "returns the show template" do
+        expect(response).to render_template :show
+      end
+
+      it "returns the correct video" do
+        expect(assigns(:video)).to eq @thor
+      end
     end
 
-    it "returns the show template" do
-      get 'show', id: @video.id
-      expect(response).to render_template :show
-    end
+    context "with no user logged in" do
+      before { subject }
 
-    it "returns the correct video" do
-      get 'show', id: @video.id
-      expect(assigns(:video)).to eq @video
+      it "redirects_to the sign_in_path" do
+        expect(response).to redirect_to sign_in_path
+        expect(flash[:danger]).to eq "Slow down there, you need to sign in first."
+      end
     end
   end
 
   describe "GET 'Search'" do
     before {
-      @video = Video.create(title: "Thor", category_id: 2)
-      @video2 = Video.create(title: "Thor: Dark World", category_id: 2)
+      @thor2 = Video.create(title: "Thor: Dark World", category_id: 2)
     }
 
-    it "returns http succss" do
-      get 'search', search: "Thor"
-      response.should be_success
-    end
+    context "with a logged in user" do
+      before {
+        session[:id] = @tony.id
+      }
 
-    it "returns the show template" do
-      get 'search', search: "Thor"
-      expect(response).to render_template :search
-    end
-
-    context "without a search term" do
-      it "returns no results" do
-        get 'search', search: ""
-        expect(assigns(:videos)).to eq []
-      end
-    end
-
-    context "with a search matching one video in the DB" do
-      it "returns one video" do
-        get 'search', search: "Thor: Dark"
-        expect(assigns(:videos)).to eq [@video2]
-      end
-    end
-
-    context "with a search matching no videos in the DB" do
-      it "returns no results" do
-        get 'search', search: "Black Widow"
-        expect(assigns(:videos)).to eq []
-      end
-    end
-
-    context "with a search matching two videos in the DB" do
-      it "returns two results" do
+      it "returns http succss" do
         get 'search', search: "Thor"
-        expect(assigns(:videos)).to eq [@video, @video2]
+        response.should be_success
+      end
+
+      it "returns the show template" do
+        get 'search', search: "Thor"
+        expect(response).to render_template :search
+      end
+
+      context "without a search term" do
+        it "returns no results" do
+          get 'search', search: ""
+          expect(assigns(:videos)).to eq []
+        end
+      end
+
+      context "with a search matching one video in the DB" do
+        it "returns one video" do
+          get 'search', search: "Thor: Dark"
+          expect(assigns(:videos)).to eq [@thor2]
+        end
+      end
+
+      context "with a search matching no videos in the DB" do
+        it "returns no results" do
+          get 'search', search: "Black Widow"
+          expect(assigns(:videos)).to eq []
+        end
+      end
+
+      context "with a search matching two videos in the DB" do
+        it "returns two results" do
+          get 'search', search: "Thor"
+          expect(assigns(:videos)).to eq [@thor, @thor2]
+        end
+      end
+    end
+
+    context "with no user logged in" do
+      it "redirects_to the sign_in_path" do
+        get 'search', search: "Thor"
+        expect(response).to redirect_to sign_in_path
+        expect(flash[:danger]).to eq "Slow down there, you need to sign in first."
       end
     end
   end
