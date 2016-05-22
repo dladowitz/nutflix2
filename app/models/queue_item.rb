9e7a7b8dbab4fa37656c584bd51aa7fdc7d5ee51
David Ladowitz <david@ladowitz.com>
2016-05-22 13:33:09 -0700

    updating queue_item to set position on create

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -2,5 +2,13 @@ class QueueItem < ActiveRecord::Base
   belongs_to :user
   belongs_to :video
 
-  validates :user, :video, :position, presence: true
+  validates :user, :video, presence: true
+
+  before_create :set_position
+
+  private
+
+  def set_position
+    self.position = user.queue_items.count + 1
+  end
 end
--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -3,7 +3,31 @@ require 'spec_helper'
 describe QueueItem do
   it { should belong_to :user }
   it { should belong_to :video }
-  it { should validate_presence_of :position }
+  # it { should validate_presence_of :position }
   it { should validate_presence_of :user }
   it { should validate_presence_of :video }
+
+  context "When a user is creating queue items" do
+    let(:user) { Fabricate(:user) }
+    let(:video) { Fabricate(:video) }
+    subject { QueueItem.create(user: user, video: video)}
+
+    it "sets active to true" do
+      expect(subject.active).to be_true
+    end
+
+    context "when the user has no other active queue items" do
+      it "sets the position to 1" do
+        expect(subject.position).to eq 1
+      end
+    end
+
+    context "when the user has one other active queue item" do
+      before { QueueItem.create(user: user, video: video) }
+
+      it "sets teh position to 2" do
+        expect(subject.position).to eq 2
+      end
+    end
+  end
 end
