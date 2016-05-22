75153191af2b690ea2060b446a1440a3ac192761
David Ladowitz <david@ladowitz.com>
2016-05-21 20:31:20 -0700

    updating seed file and video show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :full_name, :email, :password, presence: true
+  validates :full_name, :email, :password_digest, presence: true
   validates :email, uniqueness: true
 
   has_many :reviews
