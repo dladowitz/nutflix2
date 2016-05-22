dcd639cb75f3922881d855e25e5013512d09b371
David Ladowitz <david@ladowitz.com>
2016-05-21 19:53:41 -0700

    adding review controller specs

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,26 @@
 class ReviewsController < ApplicationController
   def create
-    binding.pry
-    video = Video.find_by_id review_params[:video]
-    user  = User.find_by_id review_params[:user]
+    video = Video.find_by_id review_params[:video_id]
+    user  = User.find_by_id review_params[:user_id]
 
-    @review = Review.create(video: video, user: user)
+    if video && user
+      @review = Review.new review_params
+
+      if @review.save
+        flash[:success] = "You're review has been recorded."
+      else
+        flash[:danger] = "Something went wrong. Review not saved."
+      end
+    else
+      flash[:danger] = "Something went wrong. Couldn't find video or user."
+    end
+
+    redirect_to video_path video
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :text, :user_id, :video_id)
   end
 end
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -6,21 +6,24 @@ describe ReviewsController do
     let(:user)  { Fabricate(:user) }
 
     context "with a logged in user" do
-      before {
-        # session[:id] = user.id
-        login_user user
-      }
-      # it { should change{ Review.count}.by 1 }
-      subject     { post 'create', review: { video: video, rating: 5, user: user, text: "This was a great movie"}}
+      before { login_user user }
+      subject     { post 'create', review: { video_id: video, rating: 5, user_id: user, text: "This was a great movie"}}
+
+      it { should redirect_to video_path video }
 
       it "adds a review to the DB" do
         expect{ subject }.to change{ Review.count }.by 1
-
       end
 
-      it "redirects to the current video_path"
-      it "finds the correct video"
-      it "finds the correct user"
+      it "finds the correct video" do
+        subject
+        expect(assigns(:review).video).to eq video
+      end
+
+      it "finds the correct user"  do
+        subject
+        expect(assigns(:review).user).to eq user
+      end
     end
 
     context "with no logged in user" do
