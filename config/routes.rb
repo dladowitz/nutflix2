7ab1ba0b6c7f653653c3428662d707cd3f7d2651
David Ladowitz <david@ladowitz.com>
2016-05-30 16:08:24 -0700

    updating queue items index page to hold a form for all queue items

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,10 @@ Myflix::Application.routes.draw do
 
   # Nested Resources
   resources :users, only: [:new, :create] do
-    resources :queue_items, only: [:index, :create, :destroy]
+    resources :queue_items, only: [:index, :create, :destroy] do
+      collection do
+        get "reorder"
+      end
+    end
   end
 end
