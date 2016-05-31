593b17d184ec3ce8e6341f85feb0e02298e0e7fe
David Ladowitz <david@ladowitz.com>
2016-05-31 12:36:17 -0700

    not sure if I'm closer or further than where I was before

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,7 +1,12 @@
 class PositionValidator < ActiveModel::Validator
   def validate(record)
-    unless record.position == 1
-      record.errors[:position] << 'Needs to be 1 and only 1!'
+    if record.user
+      active_items = record.user.active_queue_items
+      active_positions = active_items.pluck :position
+
+      if active_positions.include? record.position
+        record.errors[:position] << "needs to be unique across a user."
+      end
     end
   end
 end
@@ -12,8 +17,7 @@ class QueueItem < ActiveRecord::Base
   belongs_to :video
 
   validates :user, :video, presence: true
-  validates :position, uniqueness: { scope: :user }
-  # validates_with PositionValidator
+  validates_with PositionValidator
 
   before_create :set_position
   before_update :reorder_queue_positions, if: :active_changed?
--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe QueueItem do
   it { should belong_to :user }
   it { should belong_to :video }
-  # it { should validate_presence_of :position }
   it { should validate_presence_of :user }
   it { should validate_presence_of :video }
 
