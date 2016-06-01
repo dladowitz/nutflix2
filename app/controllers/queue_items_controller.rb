1c915e772569b7fc77679982b6d6e3505373ecb7
David Ladowitz <david@ladowitz.com>
2016-06-01 13:07:34 -0700

    Finally done with queue items reording. We did it. http://26.media.tumblr.com/tumblr_m16jm1BQ6O1r4xp0qo1_500.gif

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -41,48 +41,7 @@ class QueueItemsController < ApplicationController
   end
 
   def reorder
-    params[:queue].each do |queue_item_id, new_position|
-      queue_item = QueueItem.find queue_item_id
-      new_position = new_position.to_i
-      puts "Queue Item Video: #{queue_item.video.title} at position #{queue_item.position}" if @debug
-
-      if queue_item.position != new_position
-        moved_item = queue_item
-
-        if new_position > moved_item.position
-          direction = "down"
-        else
-          direction = "up"
-        end
-
-        moved_item.update_attribute(:position, new_position)
-        if direction == "down"
-          # validate_position_or_decrement()
-          non_moved_items = @user.active_queue_items.to_a
-          non_moved_items = non_moved_items.reverse  #need to start validating from the other end
-          non_moved_items.delete(moved_item)
-
-          non_moved_items.each do |item|
-            unless item.valid?
-              item.update_attribute(:position, item.position - 1)
-            end
-          end
-        else
-          # validate_position_or_increment()
-          non_moved_items = @user.active_queue_items.to_a
-          non_moved_items.delete(moved_item)
-
-          non_moved_items.each do |item|
-            unless item.valid?
-              item.update_attribute(:position, item.position + 1)
-            end
-          end
-        end
-
-        break
-      end
-    end
-
+    reorder_queue_items
 
     redirect_to user_queue_items_path @user
   end
@@ -95,5 +54,70 @@ class QueueItemsController < ApplicationController
 
   def set_user
     @user = User.find_by_id params[:user_id]
+  end
+
+  def reorder_queue_items
+    params[:queue].each do |queue_item_id, new_position|
+      moved_item = find_item_with_new_position(queue_item_id, new_position)
+
+      if moved_item
+       direction = move_direction(moved_item, new_position.to_i)
+       moved_item.update_attribute(:position, new_position)
+       validate_or_reorder(direction, moved_item)
+
+        break
+      end
+    end
+  end
+
+  def find_item_with_new_position(queue_item_id, new_position)
+    queue_item = QueueItem.find queue_item_id
+    new_position = new_position.to_i
+    puts "Queue Item Video: #{queue_item.video.title} at position #{queue_item.position}" if @debug
+
+    if queue_item.position != new_position
+      return queue_item
+    else
+      nil
+    end
+  end
+
+  def move_direction(moved_item, new_position)
+    if new_position > moved_item.position
+      direction = "down"
+    else
+      direction = "up"
+    end
+  end
+
+  def validate_or_reorder(direction, moved_item)
+    if direction == "down"
+      validate_position_or_decrement(moved_item)
+    else
+      validate_position_or_increment(moved_item)
+    end
+  end
+
+  def validate_position_or_decrement(moved_item)
+    non_moved_items = @user.active_queue_items.to_a
+    non_moved_items = non_moved_items.reverse  #need to start validating from the other end
+    non_moved_items.delete(moved_item)
+
+    non_moved_items.each do |item|
+      unless item.valid?
+        item.update_attribute(:position, item.position - 1)
+      end
+    end
+  end
+
+  def validate_position_or_increment(moved_item)
+    non_moved_items = @user.active_queue_items.to_a
+    non_moved_items.delete(moved_item)
+
+    non_moved_items.each do |item|
+      unless item.valid?
+        item.update_attribute(:position, item.position + 1)
+      end
+    end
   end
 end
