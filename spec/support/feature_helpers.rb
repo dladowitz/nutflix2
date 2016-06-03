c98726cb6b11d458fe2cfaa41e48de21e357ac56
David Ladowitz <david@ladowitz.com>
2016-06-03 15:14:18 -0700

    wrestling with update queue feature spec

---
+++ spec/support/feature_helpers.rb
@@ -0,0 +1,8 @@
+def sign_in_user_through_form(user)
+  visit sign_in_path
+  fill_in "session[email]", with: user.email
+  fill_in "session[password]", with: user.password
+  click_button "Sign in"
+
+  page.should have_content "Successful signin"
+end
